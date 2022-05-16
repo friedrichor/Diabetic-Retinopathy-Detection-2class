ad4934dded72777be8350d33835dd0d3a2b815f6
friedrichor <kongfanheng426@163.com>
2022-05-16 08:49:41 +0800

    Update params.py

--- params.py
+++ params.py
@@ -13,7 +13,8 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 num_classes = 2
 
-model = ROOT / 'weights' / 'best_model.pth'  # 保存模型路径 & predict时调用的模型
+# model = ROOT / 'weights' / 'best_model.pth'  # 保存模型路径 & predict时调用的模型
+model = ROOT / '../drive/MyDrive/weights_convnext' / 'best_model.pth'  # 保存模型路径 & predict时调用的模型
 path_train = ROOT / 'data/train/'
 path_test = ROOT / 'data/test/'
 path_json = ROOT / 'class_indices.json'
